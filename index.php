061007f5afc81508ba0437d303ef9ddb1fa6d673
krshmt <122864621+krshmt@users.noreply.github.com>
2024-01-31 21:08:38 +0100

    changement pour suivre la forme du develop

--- index.php
+++ index.php
@@ -1,98 +1,47 @@
-<?php
-include './pages/static/data.php';
-$selected = isset($_GET['searchArtist']) && $_GET['searchArtist'] == '1' ? 'artiste' : 'album';
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./pages/static/CSS/index.css">
-    <link rel="stylesheet" href="./pages/static/CSS/variables.css">
-    <link rel="stylesheet" href="./pages/static/CSS/header.css">
-    <script src="./pages/static/JS/index.js" defer></script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./pages/static/CSS/login.css" />
+    <link rel="stylesheet" href="./pages/static/CSS/variables.css" />
+    <title>Connexion - Inscription</title>
+    <script src="./pages/static/JS/login.js" defer></script>
     <script src="https://kit.fontawesome.com/b2318dca58.js" crossorigin="anonymous"></script>
-    <title>Document</title>
-</head>
-
 <body>
-    <div class="header">
-        <h1 class="header__title"><a href="./index.php"> SPOT'MUSIC</a> </h1>
-
-        <div class="account">
-            <a href="./pages/templates/login.php"><i class="fa-regular fa-user"></i></a>
-        </div>
-    </div>
-
-    <div class="main__application">
-        <div class="partie__gauche">
-            <div class="jenesaispas">
-                <div class="compte">
-                    <h2>Bienvenue, Kris</h2>
-                    <p class="premiere__lettre__pseudo">K</p>
-                </div>
-                <div class="recherche__artiste__album">
-                    <h3 class="rechercher_artiste"><a href="?searchArtist=1"><i class="fa-solid fa-magnifying-glass"></i> Rechercher un artiste</a></h3>
-                    <h3 class="rechercher_album"><a href="?searchArtist=0"><i class="fa-solid fa-magnifying-glass"></i> Rechercher un album</a></h3>
-                </div>
-            </div>
-            <div class="liste__playlist">
-                <div class="ajout__playlist">
-                    <h2><i class="fa-solid fa-list"></i> Votre bibliotèque</h2>
-                    <i class="fa-solid fa-plus" onclick="ajouterPlaylist()"></i>
-                </div>
-                <ul>
-                    <li>
-                        <a href="./pages/templates/playlist.php"> 
-                            <img src="./pages/static/images/coupDeCoeur.jpeg" alt="">
-                            <p>Coup de cœur</p>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="partie__droite">
-            <div class="barre__recherche">
-                <i class="fa-solid fa-magnifying-glass"></i>
-                <input class="barre__recherche__input" placeholder="Que souhaitez-vous rechercher ?" type="text" id="search" oninput="filterResults(event)" onkeypress="handleKeyPress(event)">
-                <i id="croixSelector" class="fa-solid" onclick="resetInputValue()"></i>
-            </div>
-
-            <main>
-                <?php if ($selected === 'artiste') : ?>
-                    <?php foreach ($dataArtistes as $artiste) : ?>
-                        <a href="./pages/templates/detail-artiste.php" class="album artiste" data-name="<?= strtolower($artiste['nom']) ?>">
-                            <?php if (is_null($artiste['img'])) : ?>
-                                <img src="./pages/static/images/default.jpg" alt="">
-                            <?php else : ?>
-                                <img src="<?= $artiste['img'] ?>" alt="">
-                            <?php endif; ?>
-                            <div class="contenu">
-                                <h3 class="test-arrow"><span><?= $artiste['nom'] ?></span></h3>
-                                <p>Artiste</p>
-                            </div>
-                        </a>
-                    <?php endforeach; ?>
-                <?php else : ?>
-                    <?php foreach ($dataAlbum as $album) : ?>
-                        <a href="./pages/templates/detail-album.php" class="album album__css" data-year="<?= strtolower($album['releaseYear']) ?>" data-title="<?= strtolower($album['title']) ?>" data-artist="<?= strtolower($album['by']) ?>">
-                            <?php if (is_null($album['img'])) : ?>
-                                <img src="./pages/static/images/default2.jpg" alt="">
-                            <?php else : ?>
-                                <img src="<?= $album['img'] ?>" alt="">
-                            <?php endif; ?>
-                            <div class="contenu__album">
-                                <h3 class="test-arrow"><span><?= $album['title'] ?></span></h3>
-                                <p><?= $album['releaseYear'] ?> - <?= $album['by'] ?></p>
-                            </div>
-                        </a>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-            </main>
-        </div>
-    </div>
+	<div class="container" id="container">
+		<div class="form-container sign-up-container">
+			<form action="{{ url_for('inscription') }}" method="post">
+				<h1>Créer un compte</h1>
+				<span>Crée ton compte en utilisant ton adresse mail</span>
+				<input name="name" type="text" placeholder="Name" />
+				<input name="mail" type="email" placeholder="Email" />
+				<input name="mdp" type="password" placeholder="Password" />
+				<button type="submit" name="action" value="inscription">S'inscrire</button>
+			</form>
+		</div>
+		<div class="form-container sign-in-container">
+			<form action="{{ url_for('connexion') }}" method="post">
+				<h1>Se connecter</h1>
+				<span>Utilise ton email pour te connecter</span>
+				<input name="mail" type="email" placeholder="Email" />
+				<input name="mdp" type="password" placeholder="Password" />
+				<button type="submit" name="action" value="connexion">Se connecter</button>
+			</form>
+		</div>
+		<div class="overlay-container">
+			<div class="overlay">
+				<div class="overlay-panel overlay-left">
+					<h1 class="titre-gradiant">Content de te revoir</h1>
+					<p>Pour rester connecter avec nous, connectes-toi en entrant tes données personnelles</p>
+					<button class="ghost" id="signIn">Se connecter</button>
+				</div>
+				<div class="overlay-panel overlay-right">
+					<h1 class="titre-gradiant">Salut !</h1>
+					<p>Si tu souhaites acheter des billets, entres tes données personnelles pour t'inscrire !</p>
+					<button class="ghost" id="signUp">S'inscrire</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
-
 </html>
---
+++ pages/templates/accueil.php
@@ -0,0 +1,98 @@
+<?php
+include '../static/data.php';
+$selected = isset($_GET['searchArtist']) && $_GET['searchArtist'] == '1' ? 'artiste' : 'album';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../static/CSS/index.css">
+    <link rel="stylesheet" href="../static/CSS/variables.css">
+    <link rel="stylesheet" href="../static/CSS/header.css">
+    <script src="../static/JS/index.js" defer></script>
+    <script src="https://kit.fontawesome.com/b2318dca58.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header">
+        <h1 class="header__title"><a href="./index.php"> SPOT'MUSIC</a> </h1>
+
+        <div class="account">
+            <a href="./pages/templates/login.php"><i class="fa-regular fa-user"></i></a>
+        </div>
+    </div>
+
+    <div class="main__application">
+        <div class="partie__gauche">
+            <div class="jenesaispas">
+                <div class="compte">
+                    <h2>Bienvenue, Kris</h2>
+                    <p class="premiere__lettre__pseudo">K</p>
+                </div>
+                <div class="recherche__artiste__album">
+                    <h3 class="rechercher_artiste"><a href="?searchArtist=1"><i class="fa-solid fa-magnifying-glass"></i> Rechercher un artiste</a></h3>
+                    <h3 class="rechercher_album"><a href="?searchArtist=0"><i class="fa-solid fa-magnifying-glass"></i> Rechercher un album</a></h3>
+                </div>
+            </div>
+            <div class="liste__playlist">
+                <div class="ajout__playlist">
+                    <h2><i class="fa-solid fa-list"></i> Votre bibliotèque</h2>
+                    <i class="fa-solid fa-plus" onclick="ajouterPlaylist()"></i>
+                </div>
+                <ul>
+                    <li>
+                        <a href="./pages/templates/playlist.php">
+                            <img src="../static/images/coupDeCoeur.jpeg" alt="">
+                            <p>Coup de cœur</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="partie__droite">
+            <div class="barre__recherche">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input class="barre__recherche__input" placeholder="Que souhaitez-vous rechercher ?" type="text" id="search" oninput="filterResults(event)" onkeypress="handleKeyPress(event)">
+                <i id="croixSelector" class="fa-solid" onclick="resetInputValue()"></i>
+            </div>
+
+            <main>
+                <?php if ($selected === 'artiste') : ?>
+                    <?php foreach ($dataArtistes as $artiste) : ?>
+                        <a href="./detail-artiste.php" class="album artiste" data-name="<?= strtolower($artiste['nom']) ?>">
+                            <?php if (is_null($artiste['img'])) : ?>
+                                <img src="../static/images/default.jpg" alt="">
+                            <?php else : ?>
+                                <img src="<?= $artiste['img'] ?>" alt="">
+                            <?php endif; ?>
+                            <div class="contenu">
+                                <h3 class="test-arrow"><span><?= $artiste['nom'] ?></span></h3>
+                                <p>Artiste</p>
+                            </div>
+                        </a>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <?php foreach ($dataAlbum as $album) : ?>
+                        <a href="./detail-album.php" class="album album__css" data-year="<?= strtolower($album['releaseYear']) ?>" data-title="<?= strtolower($album['title']) ?>" data-artist="<?= strtolower($album['by']) ?>">
+                            <?php if (is_null($album['img'])) : ?>
+                                <img src="../static/images/default2.jpg" alt="">
+                            <?php else : ?>
+                                <img src="<?= $album['img'] ?>" alt="">
+                            <?php endif; ?>
+                            <div class="contenu__album">
+                                <h3 class="test-arrow"><span><?= $album['title'] ?></span></h3>
+                                <p><?= $album['releaseYear'] ?> - <?= $album['by'] ?></p>
+                            </div>
+                        </a>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
