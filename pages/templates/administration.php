906b5462efa0274508b9098a7b84d285cef3c9ef
krshmt <122864621+krshmt@users.noreply.github.com>
2024-02-01 22:09:02 +0100

    fin du front pour l'ajout artiste/album et les stats

--- pages/templates/administration.php
+++ pages/templates/administration.php
@@ -1,6 +1,7 @@
 <!-- connexion administrateur
 admin@spotmusic.com
-admin1 -->
+admin1
+-->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,16 +29,18 @@ admin1 -->
                 <div class="contenu">
                     <h2>Ajouter un nouvel Artiste</h2>
                     <form action="ajouter_artiste.php" method="post">
-                        <label for="nom_artiste">Nom de l'artiste:</label><br>
-                        <input type="text" id="nom_artiste" name="nom_artiste"><br>
+                        <label for="nom_artiste">Nom de l'artiste</label><br>
+                        <input type="text" id="nom_artiste" name="nom_artiste" placeholder="Ex : Yeat"><br>
 
-                        <label for="biographie">Biographie:</label><br>
-                        <textarea id="biographie" name="biographie"></textarea><br>
+                        <label for="biographie">Biographie</label><br>
+                        <textarea id="biographie" name="biographie" placeholder="Ex : Reconnu dans le monde entier par ..."></textarea><br>
 
-                        <label for="photo">Photo (URL):</label><br>
-                        <input type="text" id="photo" name="photo"><br>
+                        <label for="photo">Photo</label><br>
+                        <input type="text" id="photo" name="photo" placeholder="Lien image (URL)"><br>
 
-                        <input type="submit" value="Ajouter l'Artiste">
+                        <div class="center__btn">
+                            <input type="submit" value="Ajouter l'Artiste">
+                        </div>
                     </form>
                 </div>
             </div>
@@ -45,26 +48,50 @@ admin1 -->
                 <div class="contenu">
                     <h2>Ajouter un nouvel Album</h2>
                     <form action="ajouter_album.php" method="post">
-                        <label for="titre_album">Titre de l'album:</label><br>
-                        <input type="text" id="titre_album" name="titre_album"><br>
+                        <label for="titre_album">Titre de l'album</label><br>
+                        <input type="text" id="titre_album" name="titre_album" placeholder="Ex : Lyfe"><br>
 
-                        <label for="annee_sortie">Année de sortie:</label><br>
-                        <input type="number" id="annee_sortie" name="annee_sortie"><br>
+                        <label for="annee_sortie">Année de sortie</label><br>
+                        <input type="number" id="annee_sortie" name="annee_sortie" placeholder="Ex : 2024"><br>
 
-                        <label for="genre">Genre:</label><br>
-                        <input type="text" id="genre" name="genre"><br>
+                        <label for="genre">Genre</label><br>
+                        <input type="text" id="genre" name="genre" placeholder="Ex : Rap"><br>
 
-                        <label for="id_artiste">ID de l'artiste:</label><br>
+                        <label for="id_artiste">ID de l'artiste</label><br>
                         <input type="number" id="id_artiste" name="id_artiste"><br>
 
-                        <label for="pochette">Pochette (URL):</label><br>
-                        <input type="text" id="pochette" name="pochette"><br>
+                        <label for="pochette">Pochette (URL)</label><br>
+                        <input type="text" id="pochette" name="pochette" placeholder="Lien image (URL)"><br>
 
-                        <input type="submit" value="Ajouter l'Album">
+                        <div class="center__btn">
+                            <input type="submit" value="Ajouter l'Album">
+                        </div>
                     </form>
                 </div>
             </div>
-            <div class="partie partie__statistique"></div>
+            <div class="partie partie__statistique">
+                <div class="contenu">
+                    <h2>Statistiques</h2>
+                    <div class="statistique__container">
+                        <div class="statistique">
+                            <h3>Nombre d'artistes</h3>
+                            <p>25742</p>
+                        </div>
+                        <div class="statistique">
+                            <h3>Nombre d'albums</h3>
+                            <p>4524</p>
+                        </div>
+                        <div class="statistique">
+                            <h3>Nombre de titres</h3>
+                            <p>452</p>
+                        </div>
+                        <div class="statistique">
+                            <h3>Nombre d'utilisateurs</h3>
+                            <p>42</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 </body>
