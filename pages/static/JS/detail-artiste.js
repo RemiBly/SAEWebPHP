e9148a743da63bbccd485459518cbb0b526309b1
Juliette Rousselet <juliette.roussleet@etu.univ-orleans.fr>
2024-02-15 08:35:15 +0100

    ajout button placeholder pour afficher les playlists

--- pages/static/JS/detail-artiste.js
+++ pages/static/JS/detail-artiste.js
@@ -16,3 +16,23 @@ function changementCoeur(id) {
         console.error("L'élément avec l'ID " + id + " n'a pas été trouvé.");
     }
 }
+
+function afficherPlaylists(playlists) {
+    let elem = document.getElementById("playlists");
+    elem.innerHTML = "";
+    playlists.forEach(function (playlist) {
+        let div = document.createElement("div");
+        div.classList.add("col-12", "col-md-6", "col-lg-4", "col-xl-3");
+        div.innerHTML = `
+            <div class="card">
+                <img src="${playlist.cover}" class="card-img-top" alt="${playlist.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${playlist.name}</h5>
+                    <p class="card-text">${playlist.description}</p>
+                    <a href="/playlist/${playlist.id}" class="btn btn-primary">Voir la playlist</a>
+                </div>
+            </div>
+        `;
+        elem.appendChild(div);
+    });
+}
